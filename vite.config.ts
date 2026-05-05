0c66a7322bca4c6b8b1bb4dfa6e97b88c39eef9f
irungucodes <d.irungu@tierdata.co.ke>
2026-05-05 10:56:22 +0300

    hotfix: adjust token expiery time

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    strictPort: true,
+    allowedHosts: [
+      'opsradar.tekvancesolutions.co.ke'
+    ]
+  }
 })
